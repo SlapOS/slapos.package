5a37d0558cd45e59c2f83fbbcca21d5d178612f6
Łukasz Nowak <luke@nexedi.com>
2017-04-26 16:27:37 +0200

    playbook: Add tests for standalone-shared
    Those tests are used by deploy-test ERP5TestNode testsuite.

---
+++ playbook/roles/standalone-shared/tests/testSiteHttps.py
@@ -0,0 +1,29 @@
+import requests
+import unittest
+import os
+
+
+class TestSiteHttps(unittest.TestCase):
+  """Check that configuration generated in the machine works"""
+  def setUp(self):
+    self.http_url = os.environ['TEST_ACCESS_URL_HTTP']
+    self.https_url = os.environ['TEST_ACCESS_URL_HTTPS']
+
+  def test_https_erp5_login_form(self):
+    """Check that accessing login_form over HTTPS works"""
+    result = requests.get(self.https_url + '/erp5/login_form', verify=False)
+    self.assertTrue(result.ok)
+    self.assertFalse(result.is_redirect)
+    self.assertFalse(result.is_permanent_redirect)
+    self.assertEqual(result.status_code, 200)
+    self.assertTrue('ERP5 Free Open Source ERP and CRM' in result.text)
+
+  def test_https_erp5(self):
+    """Check that accessing site over HTTPS redirects to login_form"""
+    result = requests.get(
+        self.https_url + '/erp5/', verify=False, allow_redirects=False)
+    self.assertTrue(result.ok)
+    self.assertTrue(result.is_redirect)
+    self.assertFalse(result.is_permanent_redirect)
+    self.assertEqual(result.status_code, 302)
+    self.assertTrue(result.headers['Location'].endswith('/erp5/login_form'))
