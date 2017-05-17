f01aae9186f93e2a866b4b02c28111ecec7ed31e
Łukasz Nowak <luke@nexedi.com>
2017-05-17 11:45:11 +0200

    playbook: Improve HTTP test
    Do not redirect during request, just assert that redirection is
    correctly returned form the server.

--- playbook/roles/standalone-shared/tests/testSiteHttp.py
+++ playbook/roles/standalone-shared/tests/testSiteHttp.py
@@ -11,10 +11,12 @@ class TestSiteHttp(unittest.TestCase):
 
   def test_http_erp5_login_form(self):
     """Check that accessing site over HTTP redirects to HTTPS"""
-    result = requests.get(self.http_url + '/erp5/login_form', verify=False)
+    result = requests.get(
+        self.http_url + '/erp5/login_form', verify=False,
+        allow_redirects=False)
     self.assertTrue(result.ok)
     self.assertTrue(result.is_redirect)
-    self.assertTrue(result.is_permanent_redirect)
+    self.assertFalse(result.is_permanent_redirect)
     self.assertEqual(result.status_code, 302)
     self.assertTrue(result.headers['Location'].endswith('/erp5/login_form'))
     self.assertTrue(result.headers['Location'].startswith('https://'))
