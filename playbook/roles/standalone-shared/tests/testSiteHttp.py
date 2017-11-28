d96e1b04e4b82d484fc12d1a1beee261f53e9b6e
Łukasz Nowak <luke@nexedi.com>
2017-11-28 11:05:06 +0100

    playbook: Increase test result readability
    By using dict comparision with assertEqual the result will be presented
    as diff between expected and found values. This allows test reader to
    understand the reason of failing test, instead of trying to guess what
    is really wrong with the deployed site.

--- playbook/roles/standalone-shared/tests/testSiteHttp.py
+++ playbook/roles/standalone-shared/tests/testSiteHttp.py
@@ -8,18 +8,30 @@ class TestSiteHttp(unittest.TestCase):
   def setUp(self):
     self.http_url = os.environ['TEST_ACCESS_URL_HTTP']
     self.https_url = os.environ['TEST_ACCESS_URL_HTTPS']
+    self.http_url_norm = self.http_url.replace(':10080', '')
+    self.https_url_norm = self.https_url.replace(':10443', '')
 
   def test_http_erp5_login_form(self):
     """Check that accessing site over HTTP redirects to HTTPS"""
     result = requests.get(
         self.http_url + '/erp5/login_form', verify=False,
         allow_redirects=False)
-    self.assertTrue(result.ok)
-    self.assertTrue(result.is_redirect)
-    self.assertFalse(result.is_permanent_redirect)
-    self.assertEqual(result.status_code, 302)
-    self.assertTrue(result.headers['Location'].endswith('/erp5/login_form'))
-    self.assertTrue(result.headers['Location'].startswith('https://'))
+    self.assertEqual(
+      dict(
+        ok=result.ok,
+        is_redirect=result.is_redirect,
+        is_permanent_redirect=result.is_permanent_redirect,
+        status_code=result.status_code,
+        location=result.headers['Location']
+      ),
+      dict(
+        ok=True,
+        is_redirect=True,
+        is_permanent_redirect=False,
+        status_code=302,
+        location=self.https_url_norm + '/erp5/login_form'
+      )
+    )
 
   def test_http_erp5_anydomain(self):
     """Checks that any domain can be used"""
@@ -27,11 +39,19 @@ class TestSiteHttp(unittest.TestCase):
         self.http_url + '/erp5/', allow_redirects=False,
         headers={'Host': 'anyhost'}
         )
-    self.assertTrue(result.ok)
-    self.assertTrue(result.is_redirect)
-    self.assertFalse(result.is_permanent_redirect)
-    self.assertEqual(result.status_code, 302)
     self.assertEqual(
-        result.headers['Location'],
-        'https://anyhost/erp5/'
+      dict(
+        ok=result.ok,
+        is_redirect=result.is_redirect,
+        is_permanent_redirect=result.is_permanent_redirect,
+        status_code=result.status_code,
+        location=result.headers['Location']
+      ),
+      dict(
+        ok=True,
+        is_redirect=True,
+        is_permanent_redirect=False,
+        status_code=302,
+        location='https://anyhost/erp5/'
+      )
     )
--- playbook/roles/standalone-shared/tests/testSiteHttps.py
+++ playbook/roles/standalone-shared/tests/testSiteHttps.py
@@ -6,27 +6,48 @@ import os
 class TestSiteHttps(unittest.TestCase):
   """Check that configuration generated in the machine works"""
   def setUp(self):
-    self.http_url = os.environ['TEST_ACCESS_URL_HTTP']
     self.https_url = os.environ['TEST_ACCESS_URL_HTTPS']
+    self.https_url_norm = self.https_url.replace(':10443', '')
 
   def test_https_erp5_login_form(self):
     """Check that accessing login_form over HTTPS works"""
     result = requests.get(self.https_url + '/erp5/login_form', verify=False)
-    self.assertTrue(result.ok)
-    self.assertFalse(result.is_redirect)
-    self.assertFalse(result.is_permanent_redirect)
-    self.assertEqual(result.status_code, 200)
+    self.assertEqual(
+      dict(
+        ok=result.ok,
+        is_redirect=result.is_redirect,
+        is_permanent_redirect=result.is_permanent_redirect,
+        status_code=result.status_code
+      ),
+      dict(
+        ok=True,
+        is_redirect=False,
+        is_permanent_redirect=False,
+        status_code=200
+      )
+    )
     self.assertTrue('ERP5 Free Open Source ERP and CRM' in result.text)
 
   def test_https_erp5(self):
     """Check that accessing site over HTTPS redirects to login_form"""
     result = requests.get(
         self.https_url + '/erp5/', verify=False, allow_redirects=False)
-    self.assertTrue(result.ok)
-    self.assertTrue(result.is_redirect)
-    self.assertFalse(result.is_permanent_redirect)
-    self.assertEqual(result.status_code, 302)
-    self.assertTrue(result.headers['Location'].endswith('/erp5/login_form'))
+    self.assertEqual(
+      dict(
+        ok=result.ok,
+        is_redirect=result.is_redirect,
+        is_permanent_redirect=result.is_permanent_redirect,
+        status_code=result.status_code,
+        location=result.headers['Location']
+      ),
+      dict(
+        ok=True,
+        is_redirect=True,
+        is_permanent_redirect=False,
+        status_code=302,
+        location=self.https_url_norm + '/erp5/login_form'
+      )
+    )
 
   @unittest.skip(
       'Currently HTTPS will reply with "Hostname 172.16.0.9 provided via SNI '
@@ -37,11 +58,19 @@ class TestSiteHttps(unittest.TestCase):
         self.https_url + '/erp5/', verify=False, allow_redirects=False,
         headers={'Host': 'anyhost'}
         )
-    self.assertTrue(result.ok)
-    self.assertTrue(result.is_redirect)
-    self.assertFalse(result.is_permanent_redirect)
-    self.assertEqual(result.status_code, 302)
     self.assertEqual(
-        result.headers['Location'],
-        'https://anyhost/erp5/login_form'
+      dict(
+        ok=result.ok,
+        is_redirect=result.is_redirect,
+        is_permanent_redirect=result.is_permanent_redirect,
+        status_code=result.status_code,
+        location=result.headers['Location']
+      ),
+      dict(
+        ok=True,
+        is_redirect=True,
+        is_permanent_redirect=False,
+        status_code=302,
+        location='https://anyhost/erp5/login_form'
+      )
     )
