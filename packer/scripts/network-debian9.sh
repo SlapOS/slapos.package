8da6c799645778640252c1fca031999b47f83c2e
Alain Takoudjou <alain.takoudjou@nexedi.com>
2018-02-08 13:07:06 +0100

    packer: debian 9 boot with mem hotplug enabled

--- packer/scripts/network-debian9.sh
+++ packer/scripts/network-debian9.sh
@@ -4,8 +4,9 @@
 apt-get install -y net-tools
 
 # change default interface name to eth0 schema
+# enable memory hotplug auto configuration
 
-sed -i 's#GRUB_CMDLINE_LINUX=.*#GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"#' /etc/default/grub
+sed -i 's#GRUB_CMDLINE_LINUX=.*#GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 memhp_default_state=online"#' /etc/default/grub
 
 update-grub
 
