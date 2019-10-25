61ee7af3e074ae2f33974e0e0c919cc316883bd4
Thomas Gambier <thomas.gambier@nexedi.com>
2019-10-25 15:15:08 +0200

    Update debian 10 image

--- packer/scripts/network-debian.sh
+++ packer/scripts/network-debian.sh
@@ -3,8 +3,9 @@
 # provide ifconfig command
 apt-get install -y net-tools
 
-# change default interface name to eth0 schema
-# enable memory hotplug auto configuration
+# Starting from Debian9 primary interface is not named eth0 anymore
+# so change default interface name to eth0 schema.
+# Also, enable memory hotplug auto configuration
 
 sed -i 's#GRUB_CMDLINE_LINUX=.*#GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 memhp_default_state=online"#' /etc/default/grub
 
