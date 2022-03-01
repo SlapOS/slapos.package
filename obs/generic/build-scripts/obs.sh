997d1f17806706e259e629dbbe34115d72001ded
Ophelie <ophelie.gagnard@nexedi.com>
2022-03-01 18:01:45 +0100

    obs/generic/: Save the path of the local BUILD_DIRECTORY. Fix and tweak some things.
    build-scripts/obs.sh: Fix a bug. Save the path of the local BUILD_DIRECTORY so that it can be replaced in the relevant files by the right path in OBS' VM.
    dependencies.txt: Add a dependency.

--- obs/generic/build-scripts/obs.sh
+++ obs/generic/build-scripts/obs.sh
@@ -3,14 +3,21 @@ set -e
 
 source build-scripts/configuration_information.sh
 
+# temporary directory for the files needed by OBS
 TMP_DIR=$INITIAL_DIR/tmp/
 TMP_DIR=`realpath -m $TMP_DIR`
 echo TMP_DIR = $TMP_DIR
-mkdir -p $TMP_DIR
-# clean the build directory for OBS
-rm -rf $RUN_BUILDOUT_DIR/parts/
-# prepare the files for OBS
 
+# copy the Makefile at the root of the tarball
+cp $TEMPLATE_DIR/Makefile tarballs/$SOFTWARE/Makefile
+# save the local BUILD_DIR to replace it with the BUILD_DIR of OBS' VM
+echo $BUILDIR > tarball/$SOFTWARE/cache_creation_build_directory
+
+# clean the parts directory
+rm -rf $RUN_BUILDOUT_DIR/parts/
+
+## prepare the files for OBS
+mkdir -p $TMP_DIR
 # -C option allows to give tar an absolute path without archiving the directory from / (i.e. home/user/[...])
 tar czf $TMP_DIR/$SOFTWARE.tar.gz -C tarballs/ $SOFTWARE/
 tar czf $TMP_DIR/debian.tar.gz -C $DIST_DIR/ debian/
