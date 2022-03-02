116cfdd9722e5efe8d65480f87c0861bdfe5713e
Ophelie <ophelie.gagnard@nexedi.com>
2022-03-02 10:21:18 +0100

    obs/generic/: Use actual templates. Fix some bugs.
    templates/Makefile.in:
    - Fix a bug:
    	Transform templates/Makefile in an actual template Makefile.in leaving less room for path inconsistencies.
    - Fix a bug. Remove tmp-networkcached-related commands.
    
    templates/buildout*.in:
    - Fix a bug:
    	Add the extends-cache and download-cache variables.
    
    build-scripts/template_stage.sh:
    	Add support for the new templates/Makefile.in

--- obs/generic/build-scripts/template_stage.sh
+++ obs/generic/build-scripts/template_stage.sh
@@ -9,3 +9,4 @@ cd $TEMPLATE_DIR
 mkdir -p tmp/
 sed $ALL_REGEX buildout_with_gcc.cfg.in > tmp/buildout_with_gcc.cfg
 sed $ALL_REGEX buildout_without_gcc.cfg.in > tmp/buildout_without_gcc.cfg
+sed $ALL_REGEX Makefile.in > tmp/Makefile
