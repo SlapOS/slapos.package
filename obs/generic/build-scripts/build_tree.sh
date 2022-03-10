31c71fd28e7cca736b6787a2bf7208cb9ac50a40
Ophelie <ophelie.gagnard@nexedi.com>
2022-03-10 16:34:58 +0100

    obs/generic/: Support  bin/buildout backup. Tweak some variables.
    build-scripts/:
    - Backup bin/buildout and restore it so that it is not "rebootstraped" when sent to OBS.
    - Change some variable names, add a sed regex.

--- obs/generic/build-scripts/build_tree.sh
+++ obs/generic/build-scripts/build_tree.sh
@@ -7,9 +7,9 @@ cd $INITIAL_DIR
 mkdir -p $TARBALL_DIR
 # Re-downloading the software release to pack
 #cp -r /home/test/other-projects/model/1wip_simplify-fluentbit_my-slapos .
-cp -r /home/test/other-projects/new_slapos.package/sid.fluentbit.my-slapos $TARBALL_DIR
-#mv 1wip_simplify-fluentbit_my-slapos software_release
-mv $TARBALL_DIR/sid.fluentbit.my-slapos $TARBALL_DIR/software_release
+cp -r /home/test/other-projects/model/mca.my-slapos $TARBALL_DIR/software_release
+#cp -r /home/test/other-projects/new_slapos.package/sid.fluentbit.my-slapos $TARBALL_DIR
+#mv $TARBALL_DIR/mca.my-slapos $TARBALL_DIR/software_release
 
 cd $INITIAL_DIR
 # Peparing the build directories
