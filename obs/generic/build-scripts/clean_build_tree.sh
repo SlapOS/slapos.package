7103287f4d39bb1c8d512eab3ee9a6cfe24764e8
Ophelie <ophelie.gagnard@nexedi.com>
2022-03-16 14:22:53 +0100

    obs/generic/: Support fluentbit-plugin-wendelin as a separate tarball. Fix some bugs and update files.
    obs/generic/templates/compilation-templates/mca/Makefile.in: Support fluentbit-plugin-wendelin as a separate tarball.
    
    obs/generic/: Fix some bugs. Update files.
    
    build-scripts/clean_build_tree.sh: Fix failure when $RUN_BUILDOUT_DIR/go did not exist.
    build-scripts/bootstrap_buildout.sh: Fix a bug in the backup mechanism.
    templates/:
    - Fix mca/Makefile.in
    - Change debian_defaults/dirs.in to fit the mca case.

--- obs/generic/build-scripts/clean_build_tree.sh
+++ obs/generic/build-scripts/clean_build_tree.sh
@@ -5,9 +5,11 @@ source build-scripts/configuration_information.sh
 
 cd $INITIAL_DIR
 # For some reason the user does not have the "write" permission on some directories of go/. As it is
-# needed on a directory to delete a file in it, the following line add the permissions to every
+# needed on a directory to delete a file in it, the if block add the permissions to every
 # directories in go/.
-find $RUN_BUILDOUT_DIR/go -name "*" -type d -exec chmod u+xw {} +
+if [ -d $RUN_BUILDOUT_DIR/go ]; then
+	find $RUN_BUILDOUT_DIR/go -name "*" -type d -exec chmod u+xw {} +
+fi
 rm -rf $TARBALL_DIR
 
 exit # TODO: separate the tree building from the software release retrieving
