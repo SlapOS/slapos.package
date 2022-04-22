6e7e087407a86061ddfa3bf90a40783c4f3302bf
Thomas Gambier <thomas.gambier@nexedi.com>
2022-04-22 10:55:15 +0200

    Build slapos-node using only python3
    This commit also simplifies the preparation of the cache for OBS compilation.

--- obs/slapos/prepare_download_cache.sh
+++ obs/slapos/prepare_download_cache.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 source release_configuration.sh
 
 ##########
@@ -9,7 +11,7 @@ source release_configuration.sh
 TARGET_DIRECTORY=/opt/slapos
 BUILD_ROOT_DIRECTORY="$CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/build"
 BUILD_DIRECTORY=$BUILD_ROOT_DIRECTORY$TARGET_DIRECTORY
-BUILDOUT_VERSION="2.5.2+slapos013"
+BUILDOUT_VERSION="2.7.1+slapos019"
 
 rm -rf $BUILD_ROOT_DIRECTORY
 
@@ -17,44 +19,53 @@ echo "Preparing source tarball (recipe version: $RECIPE_VERSION)"
 echo " Build Directory: $BUILD_DIRECTORY "
 echo " Buildroot Directory: $BUILD_ROOT_DIRECTORY "
 
-mkdir -p $BUILD_DIRECTORY/{eggs,extends-cache,download-cache,download-cache/dist,tmp-networkcached/eggs}
+########################################################
+# build the package once keeping every source in cache #
+########################################################
 
-set -e
-
-echo "Preparing networkcached zc.buildout"
-NETWORKCACHED_DIRECTORY=$BUILD_DIRECTORY/tmp-networkcached
-sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../networkcached.cfg.in > $NETWORKCACHED_DIRECTORY/buildout.cfg
-cd $NETWORKCACHED_DIRECTORY
-# Download  bootstrap file
-wget https://bootstrap.pypa.io/bootstrap-buildout.py -O bootstrap.py
-(python2.7 -S bootstrap.py \
-  --buildout-version $BUILDOUT_VERSION \
-  --setuptools-version 44.1.1 \
-  --setuptools-to-dir eggs \
-  -f http://www.nexedi.org/static/packages/source/slapos.buildout/ && \
-./bin/buildout -v)
-
-# build locally everything with gcc
-sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../buildout_with_gcc.cfg.in > $BUILD_DIRECTORY/buildout.cfg
-
-# Build first time to get download-cache and extends-cache ready
 cd $BUILD_DIRECTORY
+mkdir -p eggs extends-cache download-cache/dist
 
-echo "$BUILD_ROOT_DIRECTORY" > $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/original_directory
+# 1) boostrap with old version
+echo "bootsrapping buildout"
+sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../slapos.buildout.cfg.in > buildout.cfg 
 
-($NETWORKCACHED_DIRECTORY/bin/buildout bootstrap  --buildout-version $BUILDOUT_VERSION \
-                        --setuptools-to-dir eggs \
-                        -f http://www.nexedi.org/static/packages/source/slapos.buildout/ && \
-    ./bin/buildout -v) || (./bin/buildout -v || (echo "Failed to run buildout, exiting." && exit 1))
+wget https://lab.nexedi.com/nexedi/slapos.buildout/raw/master/bootstrap/bootstrap.py
+python3 -S bootstrap.py \
+  --setuptools-version 40.8.0 \
+  --setuptools-to-dir eggs
 
-# remove all files from build keeping only caches
+# 2) get newest version of zc.buildout and setuptools
+#    note that we can't directly do setuptools + zc.buildout +
+#    slapos.libnetworkcache because buildout would be relaunched in the middle
+#    without the "-S" option to python
+echo "downloading good version of setuptools and zc.buildout"
+sed -i '1s/$/ -S/' bin/buildout
+sed -i "/def _satisfied(/s/\(\bsource=\)None/\11/" eggs/zc.buildout-*/zc/buildout/easy_install.py # no wheel
+bin/buildout buildout:newest=true -v
+ls download-cache/dist/*.whl && { echo "There shouldn't be any wheel in download-cache" ; exit 1 ; }
+
+# 3) compile very simple buildout with networkcache
+echo "Preparing networkcached zc.buildout"
+sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../networkcached.cfg.in > buildout.cfg 
+sed -i '1s/$/ -S/' bin/buildout
+bin/buildout buildout:newest=true -v
+
+# 4) build locally everything with gcc to get download-cache and extends-cache ready
+echo "Launch the big buildout to compile everything"
+sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../buildout_with_gcc.cfg.in > buildout.cfg
+
+bin/buildout buildout:newest=true -v
+
+###################################################
+# remove all files from build keeping only caches #
+###################################################
+
 echo "Deleting unecessary files to reduce source tarball size"
 
 # TODO: Figure out why there is no write permission even for
 #       the owner
 chmod -R u+w .
-
-cp -R eggs/slapos.rebootstrap* $BUILD_ROOT_DIRECTORY/..
 
 rm -fv .installed.cfg environment.*
 rm -rfv ./{downloads,parts,eggs,develop-eggs,bin,rebootstrap}
@@ -62,18 +73,23 @@ rm -rfv ./{downloads,parts,eggs,develop-eggs,bin,rebootstrap}
 # Removing empty directories
 find . -type d -empty -prune -exec rmdir '{}' ';'
 
-mkdir -p $BUILD_DIRECTORY/eggs
-$NETWORKCACHED_DIRECTORY/bin/buildout bootstrap --buildout-version $BUILDOUT_VERSION \
-                       --setuptools-to-dir eggs \
-                       -f http://www.nexedi.org/static/packages/source/ \
-                       -f http://www.nexedi.org/static/packages/source/slapos.buildout/ 
-
-cp -R $BUILD_ROOT_DIRECTORY/../slapos.rebootstrap* eggs
-
 # Removing Python byte-compiled files (as it will be done upon
 # package installation) and static libraries
 find . -regextype posix-extended -type f \
 	-iregex '.*/*\.(py[co]|[l]?a|exe|bat)$$' -exec rm -fv '{}' ';'
 
+#TODO remove git files
+
+##################################
+# prepare compilation inside OBS #
+##################################
+# we need the very first bootstrap script
+cp $CURRENT_DIRECTORY/../re6st/bootstrap $BUILD_DIRECTORY
+
+# we need the original directory to do a sed inside OBS
+# TODO remove this and properly use extends-cache instead
+echo "$BUILD_ROOT_DIRECTORY" > $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/original_directory
+
 # in OBS build, don't force gcc build
 sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../buildout_without_gcc.cfg.in > $BUILD_DIRECTORY/buildout.cfg
+
