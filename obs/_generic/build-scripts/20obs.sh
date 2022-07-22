f04faddf17f049ee17883715b132170e2aa7fc49
Ophelie <ophelie.gagnard@nexedi.com>
2022-07-22 17:10:38 +0200

    obs/generic: Add generic OBS packaging.
    lunzip is used as a basic example.
    
    The distribution file debian/copyright is not supported. For further
    information, please refer to:
    https://www.debian.org/doc/manuals/maint-guide/dreq.en.html#copyright

---
+++ obs/_generic/build-scripts/20obs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+source _generic/build-scripts/00env.sh
+
+# DISTRIBUTION FILES
+
+cd "$INITIAL_DIR"
+### Prepare the distribution files in $OBS_DIR
+osc checkout "$OBS_PROJECT" "$OBS_PACKAGE" || true
+rm -rf "$OBS_DIR"/*
+osc update "$OBS_DIR"
+
+# copy compilation files and override the files from _generic
+# with the one from <software_name>
+copy_and_solve_templates "$DISTRIB_FILES_GENERIC_DIR" "$OBS_DIR"
+copy_and_solve_templates "$DISTRIB_FILES_SOFTWARE_DIR" "$OBS_DIR"
+
+# ARCHIVES FILES
+
+cd "$INITIAL_DIR"
+### Finalize the tarball directory preparation
+# save the local $TARBALL_DIR path so that it is replaced by the $TARBALL_DIR path from OBS' VM
+echo "$TARBALL_DIR" > "$TARBALL_DIR"/local_tarball_directory_path
+# add a stamp so that OBS does not clean the local preparation before compiling
+touch "$TARBALL_DIR/clean-stamp"
+# restore bin/buildout
+# note: when installing python, buildout "rebootstraps" itself to use the installed python:
+# it modifies its own shebang, which would fail on OBS' VM (no such file or directory)
+if [ -f "$RUN_BUILDOUT_DIR"/bin/backup.buildout ]; then
+	mv "$RUN_BUILDOUT_DIR"/bin/backup.buildout "$RUN_BUILDOUT_DIR"/bin/buildout
+fi
+# clean the compilation related files
+rm -rf "$RUN_BUILDOUT_DIR"/{.installed.cfg,parts}
+
+cd "$INITIAL_DIR"
+### Prepare the archives for OBS
+# -C option allows to give tar an absolute path without archiving the directory from / (i.e. home/user/[...])
+tar czf "$OBS_DIR/$SOFTWARE_AND_VERSION$ARCHIVE_EXT" -C "$INITIAL_DIR/_tarballs" "$SOFTWARE_AND_VERSION"
+tar czf "$OBS_DIR/debian$ARCHIVE_EXT" -C "$OBS_DIR" debian
+
+# OBS COMMIT
+
+cd "$OBS_DIR"
+osc add *.dsc *"$ARCHIVE_EXT"
+if [ -n "$OBS_COMMIT_MSG" ]; then
+	osc commit -m "$OBS_COMMIT_MSG"
+else
+	osc commit
+fi
