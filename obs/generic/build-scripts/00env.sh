7dd32610f89ea5da406fdc16255932e1aceaca11
Ophelie <ophelie.gagnard@nexedi.com>
2022-07-25 11:38:47 +0200

    obs/generic/: Add genericity for custom environment.

--- obs/generic/build-scripts/00env.sh
+++ obs/generic/build-scripts/00env.sh
@@ -3,47 +3,52 @@
 ####################################################
 
 INITIAL_DIR=$(pwd)/
+INITIAL_DIR=$(realpath -m "$INITIAL_DIR")
 
-### DEBIAN_REVISION INFORMATION ###
-# Modify the following variables accordingly
-SOFTWARE_VERSION=1
-DEBIAN_REVISION=1
-SOFTWARE_NAME=lunzip
+source "$INITIAL_DIR"/build-scripts/custom-env/"${NAME}"_env.sh
 
 # For the version format, see: https://www.debian.org/doc/manuals/debian-faq/pkg-basics.en.html#pkgname
 # here, in <foo>_<VersionNumber>-<DebianRevisionNumber>_<DebianArchitecture>.deb
 # VersionNumber is SOFTWARE_VERSION, DebianRevisionNumber is DEBIAN_REVISION
 # note: the architecture is added when building the package (here: in OBS)
-COMPOUND_VERSION="${SOFTWARE_VERSION}-${DEBIAN_REVISION}"
-SOFTWARE_AND_VERSION="${SOFTWARE_NAME}_${COMPOUND_VERSION}"
-ARCHIVE_EXT=.tar.gz
+COMPOUND_VERSION=${COMPOUND_VERSION:-${SOFTWARE_VERSION}-${DEBIAN_REVISION}}
+SOFTWARE_AND_VERSION=${SOFTWARE_AND_VERSION:-${SOFTWARE_NAME}_${COMPOUND_VERSION}}
+ARCHIVE_EXT=${ARCHIVE_EXT:-.tar.gz}
 
 ### INSTALL INFORMATION ###
 # This is the directory you want your package to install your software in.
 # Use "/" for building an official distribution package.
-TARGET_DIR="/"
+TARGET_DIR="${TARGET_DIR:-/opt/$SOFTWARE_AND_VERSION}"
 
 ### GETTING BUILDOUT FILES ###
-GIT_REPOSITORY=https://lab.nexedi.com/nexedi/slapos
-GIT_BRANCH_OR_COMMIT=master
+GIT_REPOSITORY=${GIT_REPOSITORY:-https://lab.nexedi.com/nexedi/slapos}
+GIT_BRANCH_OR_COMMIT=${GIT_BRANCH_OR_COMMIT:-master}
 
 ### OBS AND DISTRIBUTIONS INFORMATION ###
 # Get the user from osc configuration file.
-OBS_USER="$(cat ~/.config/osc/oscrc | grep user= | cut -d'=' -f2)"
-OBS_PROJECT="home:$OBS_USER"
-OBS_DIR="$INITIAL_DIR/$OBS_PROJECT/$SOFTWARE_NAME/"
-OBS_COMMIT_MSG=""
+OBS_USER=${OBS_USER:-$(cat ~/.config/osc/oscrc | grep user= | cut -d'=' -f2)}
+OBS_PROJECT=${OBS_PROJECT:-home:$OBS_USER}
+OBS_DIR=${OBS_DIR:-$INITIAL_DIR/$OBS_PROJECT/$SOFTWARE_NAME/}
+DIST_DIR=${DIST_DIR:-$INITIAL_DIR/distribution-specifics/$SOFTWARE_NAME/}
+TARBALL_DIR=${TARBALL_DIR:-$INITIAL_DIR/tarballs/$SOFTWARE_AND_VERSION}
+
+### OBS AND DISTRIBUTIONS INFORMATION ###
+# Get the user from osc configuration file.
+OBS_USER=${OBS_USER:-$(cat ~/.config/osc/oscrc | grep user= | cut -d'=' -f2)}
+OBS_PROJECT=${OBS_PROJECT:-home:$OBS_USER}
+OBS_DIR=${OBS_DIR:-$INITIAL_DIR/$OBS_PROJECT/$SOFTWARE_NAME/}
+OBS_COMMIT_MSG=${OBS_COMMIT_MSG:-}
 
 ### BUILD INFORMATION ###
 # This is the directory with the buildout files, it can be anything but it usually is a slapos repository.
-TARBALL_DIR="$INITIAL_DIR/tarballs/$SOFTWARE_AND_VERSION"
-BUILDOUT_DIR="$TARBALL_DIR/slapos_repository/"
-BUILDOUT_ENTRY_POINT="$BUILDOUT_DIR/component/$SOFTWARE_NAME/buildout.cfg"
+TARBALL_DIR=${TARBALL_DIR:-$INITIAL_DIR/tarballs/$SOFTWARE_AND_VERSION}
+BUILDOUT_DIR=${BUILDOUT_DIR:-$TARBALL_DIR/slapos_repository/}
+BUILDOUT_ENTRY_POINT=${BUILDOUT_ENTRY_POINT:-$BUILDOUT_DIR/component/$SOFTWARE_NAME/buildout.cfg}
 
-COMPILATION_FILES_DIR="$INITIAL_DIR/additional-files/compilation/"
-BUILD_DIR="$TARBALL_DIR/build/"
-RUN_BUILDOUT_DIR="$BUILD_DIR/$TARGET_DIR"
-DISTRIB_FILES_DIR="$INITIAL_DIR/additional-files/distributions"
+COMPILATION_FILES_DIR=${COMPILATION_FILES_DIR:-$INITIAL_DIR/additional-files/compilation}
+BUILD_DIR=${BUILD_DIR:-$TARBALL_DIR/build/}
+RUN_BUILDOUT_DIR=${RUN_BUILDOUT_DIR:-$BUILD_DIR/$TARGET_DIR}
+DISTRIB_FILES_DIR=${DISTRIB_FILES_DIR:-$INITIAL_DIR/additional-files/distributions}
 
 
 ## Path normalization
@@ -58,8 +63,8 @@ RUN_BUILDOUT_DIR=$(realpath -m "$RUN_BUILDOUT_DIR")
 DISTRIB_FILES_DIR=$(realpath -m "$DISTRIB_FILES_DIR")
 
 ## Regular expressions for templates
-NAME_REGEX="s|%SOFTWARE_NAME%|$SOFTWARE_NAME|g;s|%SOFTWARE_AND_VERSION%|$SOFTWARE_AND_VERSION|g"
-VERSION_REGEX="s|%SOFTWARE_VERSION%|$SOFTWARE_VERSION|g;s|%DEBIAN_REVISION%|$DEBIAN_REVISION|g;s|%COMPOUND_VERSION%|$COMPOUND_VERSION|g"
-DIR_REGEX="s|%TARGET_DIR%|$TARGET_DIR|g;s|%BUILD_DIR%|$BUILD_DIR|g;s|%RUN_BUILDOUT_DIR%|$RUN_BUILDOUT_DIR|g"
-PATH_REGEX="s|%BUILDOUT_ENTRY_POINT%|$BUILDOUT_ENTRY_POINT|g"
-ALL_REGEX="$NAME_REGEX;$VERSION_REGEX;$DIR_REGEX;$PATH_REGEX"
+NAME_REGEX=${NAME_REGEX:-s|%SOFTWARE_NAME%|$SOFTWARE_NAME|g;s|%SOFTWARE_AND_VERSION%|$SOFTWARE_AND_VERSION|g}
+VERSION_REGEX=${VERSION_REGEX:-s|%SOFTWARE_VERSION%|$SOFTWARE_VERSION|g;s|%DEBIAN_REVISION%|$DEBIAN_REVISION|g;s|%COMPOUND_VERSION%|$COMPOUND_VERSION|g}
+DIR_REGEX=${DIR_REGEX:-s|%TARGET_DIR%|$TARGET_DIR|g;s|%BUILD_DIR%|$BUILD_DIR|g;s|%RUN_BUILDOUT_DIR%|$RUN_BUILDOUT_DIR|g}
+PATH_REGEX=${PATH_REGEX:-s|%BUILDOUT_ENTRY_POINT%|$BUILDOUT_ENTRY_POINT|g}
+ALL_REGEX=${ALL_REGEX:-$NAME_REGEX";"$VERSION_REGEX";"$DIR_REGEX";"$PATH_REGEX}
