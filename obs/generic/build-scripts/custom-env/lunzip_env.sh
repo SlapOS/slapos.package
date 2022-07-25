205492de8f0bf0d531c35824f5faefa5222af3df
Ophelie <ophelie.gagnard@nexedi.com>
2022-07-25 11:38:47 +0200

    obs/generic/: Add genericity for custom environment.

---
+++ obs/generic/build-scripts/custom-env/lunzip_env.sh
@@ -0,0 +1,19 @@
+####################################################
+# Used in build-scripts/00env.sh: source lunzip_env.sh
+####################################################
+
+### DEBIAN_REVISION INFORMATION ###
+# Modify the following variables accordingly
+SOFTWARE_VERSION=1
+DEBIAN_REVISION=1
+SOFTWARE_NAME=lunzip
+
+### INSTALL INFORMATION ###
+# This is the directory you want your package to install your software in.
+# Use "/" for building an official distribution package.
+TARGET_DIR="/"
+
+### BUILDOUT FILES AND VERSIONS ###
+BUILDOUT_ENTRY_POINT="$(pwd)/tarballs/${SOFTWARE_NAME}_${SOFTWARE_VERSION}-${DEBIAN_REVISION}/slapos_repository/component/$SOFTWARE_NAME/buildout.cfg"
+SETUPTOOLS_VERSION=44.1.1
+ZC_BUILDOUT_VERSION=2.7.1+slapos016
