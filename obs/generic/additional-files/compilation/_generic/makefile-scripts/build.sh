140992d5707664bacbd45c53b04cf2c854e695e0
Ophelie <ophelie.gagnard@nexedi.com>
2022-07-25 22:56:40 +0200

    obs/generic/: Add genericity for compilation files.

---
+++ obs/generic/additional-files/compilation/_generic/makefile-scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+source makefile-scripts/compilation-env.sh
+
+echo "Fixing buildout path to "$TARBALL_DIR" rather than "$OLD_TARBALL_DIR" for buildout"
+cd "$RUN_BUILDOUT_DIR"
+sed -i "s#$OLD_TARBALL_DIR#$TARBALL_DIR#g" buildout.cfg bin/*
+bin/buildout -v
+
+touch build-stamp
--- obs/generic/build-scripts/custom-env/lunzip_env.sh
+++ obs/generic/build-scripts/custom-env/lunzip_env.sh
@@ -12,3 +12,8 @@ SOFTWARE_NAME=lunzip
 # This is the directory you want your package to install your software in.
 # Use "/" for building an official distribution package.
 TARGET_DIR="/"
+
+### BUILDOUT FILES AND VERSIONS ###
+SETUPTOOLS_VERSION=44.1.1
+ZC_BUILDOUT_VERSION=2.7.1+slapos016
+ZC_RECIPE_EGG_VERSION=2.0.3+slapos003
