d7faa865cd47028a8fa941d2e1dbe1ec5ed093a7
Ophelie <ophelie.gagnard@nexedi.com>
2022-07-25 11:38:47 +0200

    obs/generic/: Add genericity for custom environment.

---
+++ obs/generic/build-scripts/custom-env/lunzip_env.sh
@@ -0,0 +1,9 @@
+####################################################
+# Used in build-scripts/00env.sh: source lunzip_env.sh
+####################################################
+
+### DEBIAN_REVISION INFORMATION ###
+# Modify the following variables accordingly
+SOFTWARE_VERSION=1
+DEBIAN_REVISION=1
+SOFTWARE_NAME=lunzip
