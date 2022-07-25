716df7e9c9dcdef110da7191eec06814ed129dd4
Ophelie <ophelie.gagnard@nexedi.com>
2022-07-25 22:56:40 +0200

    obs/generic/: Add genericity for compilation files.

---
+++ obs/generic/additional-files/compilation/_generic/makefile-scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+source makefile-scripts/compilation-env.sh
+
+mkdir -p "$INSTALL_DIR/bin"
+mkdir -p "$INSTALL_DIR/etc"
+mkdir -p "$INSTALL_DIR/lib"
+mkdir -p "$INSTALL_DIR/sbin"
+mkdir -p "$INSTALL_DIR/share"
+cp -r "$RUN_BUILDOUT_DIR/parts/"*/bin/* "$INSTALL_DIR/bin" || true
+cp -r "$RUN_BUILDOUT_DIR/parts/"*/etc/* "$INSTALL_DIR/etc" || true
+cp -r "$RUN_BUILDOUT_DIR/parts/"*/lib/* "$INSTALL_DIR/lib" || true
+cp -r "$RUN_BUILDOUT_DIR/parts/"*/sbin/* "$INSTALL_DIR/sbin" || true
+cp -r "$RUN_BUILDOUT_DIR/parts/"*/share/* "$INSTALL_DIR/share" || true
+
+rm -f clean-stamp
