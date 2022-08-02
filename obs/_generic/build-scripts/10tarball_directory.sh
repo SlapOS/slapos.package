8ee828ea82240a05db5719f5de88ce614754e944
Ophelie <ophelie.gagnard@nexedi.com>
2022-08-02 16:26:59 +0200

    obs/generic: Add support for compilation using downloaded Go modules.

--- obs/_generic/build-scripts/10tarball_directory.sh
+++ obs/_generic/build-scripts/10tarball_directory.sh
@@ -49,6 +49,18 @@ cp bin/buildout bin/backup.buildout
 ./bin/buildout -v
 
 cd "$INITIAL_DIR"
+### Fix the go/ directory.
+
+# For some reason the user does not have the "write" permission on some directories within $RUN_BUILDOUT_DIR/go/. As it is
+# needed on a directory to delete a file in it, the if block adds the permissions to every
+# directories in go/.
+# This is performed before copying the directory tree elsewhere so that every copy is fixed.
+# It also allows the cleaning script to delete the result of the current script.
+if [ -d "$RUN_BUILDOUT_DIR"/go ]; then
+	find "$RUN_BUILDOUT_DIR"/go -type d -exec chmod u+xw {} +
+fi
+
+cd "$INITIAL_DIR"
 ### Backup $TARBALL_DIR for debugging or other purpose
 # add "backup." before the directory name pointed to by $TARBALL_DIR
 BACKUP_DIR="$TARBALL_DIR"/../backup."$SOFTWARE_AND_VERSION"
