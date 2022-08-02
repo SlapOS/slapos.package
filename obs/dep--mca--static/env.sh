a48a16cf59fe23662cbfe6cf20032152964ac7fd
Ophelie <ophelie.gagnard@nexedi.com>
2022-08-02 16:36:51 +0200

    obs/generic: Add dep--mca--static packaging.

---
+++ obs/dep--mca--static/env.sh
@@ -0,0 +1,8 @@
+export SOFTWARE_NAME=dep--mca-static
+export MAINTAINER_NAME=Ophelie
+export MAINTAINER_EMAIL=ophelie.gagnard@nexedi.com
+export OBS_USER=oph.nxd
+export PACKAGE_BUILD_DEPENDENCIES="debhelper (>= 4.1.16), chrpath, wget, python (>= 2.7), python3 (>= 3.7), python3-dev (>= 3.7), golang (>= 2:1.15~1), cmake (>= 3.18)"
+export BUILDOUT_RELATIVE_PATH=software/mca/dep--mca--static.cfg
+export SETUPTOOLS_VERSION=44.1.1
+export ZC_BUILDOUT_VERSION=2.7.1+slapos016
