74372e21ba2df1c4b9b9a060d4fc74e1d4cd3029
Ophelie <ophelie.gagnard@nexedi.com>
2022-08-02 16:36:51 +0200

    obs/generic/: Add dep--mca--static packaging.

---
+++ obs/generic/additional-files/compilation/dep--mca--static/makefile-scripts/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+source makefile-scripts/compilation-env.sh
+
+# note: /share files such as manpages are not yet supported in this project
+mkdir -p "$INSTALL_DIR"/bin
+mkdir -p "$INSTALL_DIR"/etc
+mkdir -p "$INSTALL_DIR"/lib
+cp -r "$RUN_BUILDOUT_DIR"/parts/mca/bin/* "$INSTALL_DIR"/bin || true
+cp -r "$RUN_BUILDOUT_DIR"/parts/mca/etc/* "$INSTALL_DIR"/etc || true
+cp -r "$RUN_BUILDOUT_DIR"/parts/fluentbit-plugin-wendelin/lib/* "$INSTALL_DIR"/lib || true
