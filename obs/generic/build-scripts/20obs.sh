368a2459f4b015a344fef8488ba6bd980b5a8b47
Ophelie <ophelie.gagnard@nexedi.com>
2022-08-02 10:36:43 +0200

    obs/generic/: Add genericity for distribution files.
    The distribution file debian/copyright is not supported. For further
    information, please refer to:
    https://www.debian.org/doc/manuals/maint-guide/dreq.en.html#copyright

--- obs/generic/build-scripts/20obs.sh
+++ obs/generic/build-scripts/20obs.sh
@@ -3,18 +3,18 @@ set -e
 
 source build-scripts/00env.sh
 
+# DISTRIBUTION FILES
+
 cd "$INITIAL_DIR"
 ### Prepare the distribution files in $OBS_DIR
 osc checkout "$OBS_PROJECT" "$SOFTWARE_NAME" || true
 
-rm -f "$OBS_DIR/lunzip_$COMPOUND_VERSION.dsc"
-rm -f "$OBS_DIR/debian.tar.gz"
-rm -rf "$OBS_DIR/debian"
+copy_additional_files "$DISTRIB_FILES_DIR/_generic" "$OBS_DIR"
+mv "$OBS_DIR/_generic.dsc" "$OBS_DIR/$SOFTWARE_NAME.dsc"
+copy_additional_files "$DISTRIB_FILES_DIR/$SOFTWARE_NAME" "$OBS_DIR"
+copy_additional_files "$DISTRIB_FILES_DIR/$SOFTWARE_AND_VERSION" "$OBS_DIR"
 
-sed "$ALL_REGEX" "$DISTRIB_FILES_DIR/lunzip.dsc.in" > "$OBS_DIR/lunzip_$COMPOUND_VERSION.dsc"
-mkdir -p "$OBS_DIR/debian"
-sed "$ALL_REGEX" "$DISTRIB_FILES_DIR/debian/changelog.in" > "$OBS_DIR/debian/changelog"
-cp -r "$DISTRIB_FILES_DIR/debian/"{compat,control,copyright,dirs,rules,source} "$OBS_DIR/debian"
+# ARCHIVES FILES
 
 cd "$INITIAL_DIR"
 ### Finalize the tarball directory preparation
@@ -36,6 +36,8 @@ cd "$INITIAL_DIR"
 # -C option allows to give tar an absolute path without archiving the directory from / (i.e. home/user/[...])
 tar czf "$OBS_DIR/$SOFTWARE_AND_VERSION$ARCHIVE_EXT" -C "$INITIAL_DIR/tarballs" "$SOFTWARE_AND_VERSION"
 tar czf "$OBS_DIR/debian$ARCHIVE_EXT" -C "$OBS_DIR" debian
+
+# OBS COMMIT
 
 cd "$OBS_DIR"
 osc add *.dsc *"$ARCHIVE_EXT"
