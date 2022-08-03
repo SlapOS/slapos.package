35620f631fa883edb7972c2dbab201a46292cda8
Ophelie <ophelie.gagnard@nexedi.com>
2022-08-03 11:05:39 +0200

    obs/generic/: Add dep--fluent-bit packaging.

---
+++ obs/generic/additional-files/compilation/dep--fluent-bit/makefile-scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+source makefile-scripts/compilation-env.sh
+
+# note: /share files such as manpages are not yet supported in this project
+mkdir -p "$INSTALL_DIR"/bin
+cp -r "$RUN_BUILDOUT_DIR"/parts/fluent-bit/bin/fluent-bit "$INSTALL_DIR"/bin || true
+
+rm -f clean-stamp
