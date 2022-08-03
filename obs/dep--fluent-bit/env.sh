aaf2b766fcf4edfba7344910dbe0bdb1c9b68fbe
Ophelie <ophelie.gagnard@nexedi.com>
2022-08-03 11:05:39 +0200

    obs/generic: Add dep--fluent-bit packaging.

---
+++ obs/dep--fluent-bit/env.sh
@@ -0,0 +1,7 @@
+export SOFTWARE_NAME=dep--fluent-bit
+export MAINTAINER_NAME=Ophelie
+export MAINTAINER_EMAIL=ophelie.gagnard@nexedi.com
+export OBS_USER=oph.nxd
+export PACKAGE_BUILD_DEPENDENCIES="debhelper (>= 4.1.16), chrpath, wget, python (>= 2.7), python3 (>= 3.7), python3-dev (>= 3.7), cmake (>= 3.18)"
+export SETUPTOOLS_VERSION=44.1.1
+export ZC_BUILDOUT_VERSION=2.7.1+slapos016
