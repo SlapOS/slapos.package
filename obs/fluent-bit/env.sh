59b6705789025191c284da39e4131b57e7f0fade
Thomas Gambier <thomas.gambier@nexedi.com>
2022-09-16 14:46:36 +0200

    obs/fluent-bit: use better version of buildout (working with python 3.8)

--- obs/fluent-bit/env.sh
+++ obs/fluent-bit/env.sh
@@ -1,8 +1,8 @@
-# packahge containing fluent-bit
+# package containing fluent-bit
 
 export SOFTWARE_NAME=fluent-bit
 export MAINTAINER_NAME="Ophélie Gagnard"
 export MAINTAINER_EMAIL=ophelie.gagnard@nexedi.com
 export PACKAGE_BUILD_DEPENDENCIES="debhelper, chrpath, python3 (>=3.7) | python"
 export SETUPTOOLS_VERSION=44.1.1
-export ZC_BUILDOUT_VERSION=2.7.1+slapos016
+export ZC_BUILDOUT_VERSION=2.7.1+slapos019
