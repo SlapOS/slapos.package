5e40fb993988debfff63b12e0aade12c10d94db0
Joanne Hugé <joanne.huge@nexedi.com>
2022-09-16 13:04:21 +0000

    fixup! playbook/ors-upgrade: fix init-sdr script

--- playbook/roles/ors-upgrade/files/init-sdr.sh
+++ playbook/roles/ors-upgrade/files/init-sdr.sh
@@ -3,7 +3,7 @@
 AMARISOFT_PATH="/opt/amarisoft/$(ls -1 /opt/amarisoft | grep "^v[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$" | sort | tail -n1)"
 
 $AMARISOFT_PATH/trx_sdr/sdr_util version && exit;
-lsmod | grep -q sdr || rmmod sdr;
+lsmod | grep -q sdr && rmmod sdr;
 cd $AMARISOFT_PATH/trx_sdr/kernel;
 make clean;
 bash init.sh;
