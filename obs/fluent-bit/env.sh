b3ecd384de4ea37157bcdcedf9876c53b26b9eec
Thomas Gambier <thomas.gambier@nexedi.com>
2022-09-16 15:26:42 +0200

    obs/_generic: support bootstrapping with either python2 or python3

--- obs/fluent-bit/env.sh
+++ obs/fluent-bit/env.sh
@@ -3,6 +3,6 @@
 export SOFTWARE_NAME=fluent-bit
 export MAINTAINER_NAME="Ophélie Gagnard"
 export MAINTAINER_EMAIL=ophelie.gagnard@nexedi.com
-export PACKAGE_BUILD_DEPENDENCIES="debhelper, chrpath, python3 (>=3.7) | python"
+export PACKAGE_BUILD_DEPENDENCIES="debhelper, chrpath, python3 (>=3.7) | python, python3-dev (>= 3.7) | python-dev"
 export SETUPTOOLS_VERSION=44.1.1
 export ZC_BUILDOUT_VERSION=2.7.1+slapos016
