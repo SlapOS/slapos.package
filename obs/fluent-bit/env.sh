472688e9b9c11a1b369d08c045bf0874dce497b4
Thomas Gambier <thomas.gambier@nexedi.com>
2022-09-16 14:43:50 +0200

    obs/generic: support | in variables
    Use § for sed separator instead of | so that we can have | in variables.
    
    We need | in PACKAGE_BUILD_DEPENDENCIES of fluent-bit

--- obs/fluent-bit/env.sh
+++ obs/fluent-bit/env.sh
@@ -3,6 +3,6 @@
 export SOFTWARE_NAME=fluent-bit
 export MAINTAINER_NAME="Ophélie Gagnard"
 export MAINTAINER_EMAIL=ophelie.gagnard@nexedi.com
-export PACKAGE_BUILD_DEPENDENCIES="debhelper (>= 4.1.16), chrpath, wget, python (>= 2.7), python3 (>= 3.7), python3-dev (>= 3.7), cmake (>= 3.18)"
+export PACKAGE_BUILD_DEPENDENCIES="debhelper, chrpath, python3 (>=3.7) | python"
 export SETUPTOOLS_VERSION=44.1.1
 export ZC_BUILDOUT_VERSION=2.7.1+slapos016
