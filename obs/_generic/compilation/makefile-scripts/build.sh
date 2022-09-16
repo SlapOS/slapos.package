fd93dd5c16d44d2df3e4e337962f880b116c8530
Thomas Gambier <thomas.gambier@nexedi.com>
2022-09-16 15:26:42 +0200

    obs/_generic: support bootstrapping with either python2 or python3

--- obs/_generic/compilation/makefile-scripts/build.sh
+++ obs/_generic/compilation/makefile-scripts/build.sh
@@ -6,7 +6,8 @@ source makefile-scripts/compilation-env.sh
 
 echo "Fixing buildout path to "$TARBALL_DIR" rather than "$OLD_TARBALL_DIR" for buildout"
 cd "$RUN_BUILDOUT_DIR"
-sed -i "s#$OLD_TARBALL_DIR#$TARBALL_DIR#g" buildout.cfg bin/*
-bin/buildout -v
+sed -i "s#$OLD_TARBALL_DIR#$TARBALL_DIR#g" buildout.cfg
+$SLAPOS_BOOTSTRAP_SYSTEM_PYTHON $TARBALL_DIR/makefile-scripts/bootstrap
+$SLAPOS_BOOTSTRAP_SYSTEM_PYTHON bin/buildout -v
 
 touch clean-stamp
