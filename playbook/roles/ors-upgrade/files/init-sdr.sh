e6f90cd9f1a3912617e283077fbb53c5f1ef8198
Joanne Hugé <joanne.huge@nexedi.com>
2022-09-20 08:32:49 +0200

    ors-upgrade: fix init-sdr and wait before launching ors-upgrade at reboot

--- playbook/roles/ors-upgrade/files/init-sdr.sh
+++ playbook/roles/ors-upgrade/files/init-sdr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 AMARISOFT_PATH="/opt/amarisoft/$(ls -1 /opt/amarisoft | grep "^v[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$" | sort | tail -n1)"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 
 $AMARISOFT_PATH/trx_sdr/sdr_util version && exit;
 lsmod | grep -q sdr && rmmod sdr;
