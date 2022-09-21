979a58b2f7ddfc99a77a5e9ea31a4b99b8512547
Ophelie <ophelie.gagnard@nexedi.com>
2022-09-21 15:49:37 +0200

    obs: Integrate new bootstraping method to generic.
    Integrate to generic the new bootstraping method from ```obs/slapos```.
    Update some egg versions.

--- obs/lunzip/env.sh
+++ obs/lunzip/env.sh
@@ -8,5 +8,5 @@ export MAINTAINER_EMAIL="ophelie.gagnard@nexedi.com"
 export TARGET_DIR="/"
 export BUILDOUT_RELATIVE_PATH=component/lunzip/buildout.cfg
 export SETUPTOOLS_VERSION=44.1.1
-export ZC_BUILDOUT_VERSION=2.7.1+slapos016
+export ZC_BUILDOUT_VERSION=2.7.1+slapos019
 export OBS_PROJECT=home:oph.nxd
