17d15d8574246dc9d7928b8feb9e73bf8be1aa9b
Ophelie <ophelie.gagnard@nexedi.com>
2022-09-22 10:57:08 +0200

    obs: Improve version identification and add default OBS commit message.

--- obs/fluent-bit/env.sh
+++ obs/fluent-bit/env.sh
@@ -5,8 +5,10 @@ export MAINTAINER_EMAIL=ophelie.gagnard@nexedi.com
 
 export SOFTWARE_NAME=fluent-bit
 # version format: <fluent-bit>+<slapos>
-export SOFTWARE_VERSION=1.9.7+1.0.277
+export SOFTWARE_VERSION=1.9.7+1.0.277+dep
 export GIT_BRANCH_OR_COMMIT=1.0.277
 
 export SETUPTOOLS_VERSION=44.1.1
 export ZC_BUILDOUT_VERSION=2.7.1+slapos019
+
+export OBS_COMMIT_MSG="Yet another push in OBS."
