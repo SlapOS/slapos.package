23a596d407040cd7689d32d0a028ea2d08c65640
Ophelie <ophelie.gagnard@nexedi.com>
2022-09-26 14:35:31 +0200

    obs/fluent-bit: Use a light build dependencies version.

--- obs/fluent-bit/env.sh
+++ obs/fluent-bit/env.sh
@@ -8,6 +8,10 @@ export SOFTWARE_NAME=fluent-bit
 export SOFTWARE_VERSION=1.9.7+1.0.279+dep
 export GIT_BRANCH_OR_COMMIT=1.0.279
 
+export PACKAGE_BUILD_DEPENDENCIES="debhelper (>= 4.1.16), chrpath, wget, python (>= 2.7), python3 (>= 3.7), python3-dev (>= 3.7), golang (>= 2:1.15~1), cmake (>= 3.18)"
+export GIT_REPOSITORY=https://lab.nexedi.com/Ophelie/slapos.git
+export GIT_BRANCH_OR_COMMIT=fluent-bit_tmp
+export BUILDOUT_RELATIVE_PATH=component/fluent-bit/dep--fluent-bit.cfg
 export SETUPTOOLS_VERSION=44.1.1
 export ZC_BUILDOUT_VERSION=2.7.1+slapos019
 
