9d2d57864d0fd1118a3e67f8b5075a6561ad08d7
Ophelie <ophelie.gagnard@nexedi.com>
2022-11-15 16:04:24 +0100

    obs/mca--static: Remove useless declaration.
    ZC_BUILDOUT_VERSION variable cannot be defined in custom environments.

--- obs/mca--static/env.sh
+++ obs/mca--static/env.sh
@@ -7,7 +7,6 @@ export SOFTWARE_VERSION=0.3.1+0.2+1.0.293+dep
 
 export PACKAGE_BUILD_DEPENDENCIES="debhelper (>= 4.1.16), chrpath, wget, python (>= 2.7), python3 (>= 3.7), python3-dev (>= 3.7), golang (>= 2:1.15~1), cmake (>= 3.18)"
 export BUILDOUT_RELATIVE_PATH=software/mca/dep--mca--static.cfg
-export ZC_BUILDOUT_VERSION=2.7.1+slapos019
 
 export OBS_PROJECT=home:oph.nxd
 export OBS_COMMIT_MSG="Yet another push in OBS."
