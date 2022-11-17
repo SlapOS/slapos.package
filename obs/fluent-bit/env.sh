bb1d539829ce5167d106ce55a7546a1fa4532588
Ophelie <ophelie.gagnard@nexedi.com>
2022-11-17 18:01:41 +0100

    obs/fluent-bit: Fix build dependencies.

--- obs/fluent-bit/env.sh
+++ obs/fluent-bit/env.sh
@@ -7,7 +7,7 @@ export SOFTWARE_NAME=fluent-bit
 # version format: <fluent-bit>+<slapos>
 export SOFTWARE_VERSION=1.9.8+1.0.293+nodep
 
-export PACKAGE_BUILD_DEPENDENCIES="debhelper (>= 4.1.16), chrpath, wget, python (>= 2.7), python3 (>= 3.7), python3-dev (>= 3.7), golang (>= 2:1.15~1), cmake (>= 3.18)"
+export PACKAGE_BUILD_DEPENDENCIES="debhelper (>= 4.1.16), chrpath, wget, python (>= 2.7), python3 (>= 3.7), python3-dev (>= 3.7), cmake (>= 3.18)"
 export BUILDOUT_RELATIVE_PATH=component/fluent-bit/obs-nodep.cfg
 export ZC_BUILDOUT_VERSION=2.7.1+slapos019
 
