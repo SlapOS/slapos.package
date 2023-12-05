693a845fcd565d517698bb0f9a8e71470a69072c
Joanne Hugé <joanne.huge@nexedi.com>
2023-12-05 14:17:39 +0000

    roles/amarisoft-upgrade: add role to upgrade amarisoft binaries
    This role makes a request to shacache to check if new amarisoft
    binaries are available. If there are and if the license on the
    machine allows it the binaries will be downloaded and decrypted
    using the server's private key in /opt/private-key.
    The binaries will then be installed in
    /opt/amarisoft/vYYYY-MM-DD, to be used with ors-amarisoft
    software release.

---
+++ playbook/roles/amarisoft-upgrade/files/encrypt-data.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+usage() {
+    echo 1>&2 "Usage: $0 KEYFILE IVFILE (encrypt | decrypt | genkey) [IN] [OUT]"
+    exit 1
+}
+chacha20() {
+    test "$#" -ne 6 && usage
+    KEY="$(od -An -v -tx1 $1 |tr -d ' \n')"
+    IV="$(openssl base64 -A -in $2 -d |od -An -v -tx1 |tr -d ' \n')"
+    openssl enc $6 -chacha20 -K $KEY -iv $IV -nosalt -in $4 -out $5
+}
+
+case "$3" in
+    encrypt )
+        chacha20 $@ -e;;
+    decrypt )
+        chacha20 $@ -d;;
+    genkey )
+        test "$#" -ne 3 && usage
+        head -c32 /dev/urandom > $1
+        head -c16 /dev/urandom > $2.bin
+        openssl base64 -A -in $2.bin -out $2;;
+    * )
+        usage;;
+esac
