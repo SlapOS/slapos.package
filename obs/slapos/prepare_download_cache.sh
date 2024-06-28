becf815089033ce33b230de4a9507c2c9316ec02
Thomas Gambier <thomas.gambier@nexedi.com>
2024-06-28 09:37:20 +0200

    Adapt build with buildout3

--- obs/slapos/prepare_download_cache.sh
+++ obs/slapos/prepare_download_cache.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 source release_configuration.sh
 
@@ -23,27 +24,29 @@ echo " Buildroot Directory: $BUILD_ROOT_DIRECTORY "
 # build the package once keeping every source in cache #
 ########################################################
 
-mkdir -p $BUILD_DIRECTORY/{eggs,extends-cache,download-cache/dist}
-cd $BUILD_DIRECTORY
+mkdir -p $BUILD_DIRECTORY/{eggs,extends-cache,download-cache/dist,bootstrap-dir}
+cd $BUILD_DIRECTORY/bootstrap-dir
 
-# 1) boostrap with isolation
-echo "bootsrapping buildout"
-sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../slapos.buildout.cfg.in > buildout.cfg
-
-buildout bootstrap buildout:isolate-from-buildout-and-setuptools-path=true
-ls download-cache/dist/*.whl && { echo "There shouldn't be any wheel in download-cache" ; exit 1 ; }
+# 1) you need a recent enough version of Nexedi buildout, you can do e.g.:
+# pip install http://www.nexedi.org/static/packages/source/slapos.buildout/zc.buildout-3.0.1%2Bslapos002.tar.gz
 
 # 2) compile very simple buildout with networkcache
 echo "Preparing networkcached zc.buildout"
 sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../networkcached.cfg.in > buildout.cfg
-sed -i '1s/$/ -S/' bin/buildout
-bin/buildout buildout:newest=true -v
+# be careful to let 'extra-paths=' in buildout section for isolation
+buildout buildout:download-cache=../download-cache bootstrap
 
-# 3) build locally everything with gcc to get download-cache and extends-cache ready
-echo "Launch the big buildout to compile everything"
+# 3) bootstrap the buildout used for full compilation
+echo "Launch the bootstrap of final buildout"
+cd $BUILD_DIRECTORY
 sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../buildout_with_gcc.cfg.in > buildout.cfg
+bootstrap-dir/bin/buildout bootstrap
 
-bin/buildout buildout:newest=true -v
+# 4) build locally everything with gcc to get download-cache and extends-cache ready
+echo "Launch the big buildout to compile everything"
+bin/buildout buildout:newest=true -v | tee buildout-full.log
+
+
 
 ###################################################
 # remove all files from build keeping only caches #
@@ -68,11 +71,10 @@ find . -regextype posix-extended -type f \
 
 #TODO remove git files
 
+
 ##################################
 # prepare compilation inside OBS #
 ##################################
-# we need the very first bootstrap script
-cp $CURRENT_DIRECTORY/../_generic/compilation/makefile-scripts/bootstrap $BUILD_DIRECTORY
 
 # we need the original directory to do a sed inside OBS
 # TODO remove this and properly use extends-cache instead
@@ -80,4 +82,5 @@ echo "$BUILD_ROOT_DIRECTORY" > $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/origi
 
 # in OBS build, don't force gcc build
 sed  "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s|\%PATCHES_DIRECTORY\%|$PATCHES_DIRECTORY|g;s|\%TARGET_DIRECTORY\%|$TARGET_DIRECTORY|g;s|\%BUILD_ROOT_DIRECTORY\%|$BUILD_ROOT_DIRECTORY|g;s|\%BUILD_DIRECTORY\%|$BUILD_DIRECTORY|g" $BUILD_ROOT_DIRECTORY/../buildout_without_gcc.cfg.in > $BUILD_DIRECTORY/buildout.cfg
+
 
