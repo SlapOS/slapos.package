b8bee4b2c38ecc394116407ad6dbffc463bd7656
Thomas Gambier <thomas.gambier@nexedi.com>
2024-10-04 18:40:00 +0200

    obs/re6st: use python3 to build re6st-node
    Also adapt the bootstrap to buildout3.

---
+++ obs/re6st/build_python3_if_needed.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# This script compiles python3 if current python3 is older than python3.7 (minimum version required by buildout)
+# It returns the path of the python3 executable to use
+
+python3 -c 'import sys; sys.exit(1) if sys.version_info < (3, 7) else print(sys.executable)' && exit || true
+
+build_dir=.build
+ark=$PWD/opt/re6st/python3.tar.xz
+mkdir -p $build_dir
+cd $build_dir
+tar --strip-components=1 -xaf "$ark"
+./configure >/dev/null 2>&1
+make -j 4 >/dev/null 2>&1
+
+readlink -f python
